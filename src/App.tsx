ccaa97d818fe6d15c95c97e7b23b3fd19561a4bf
Anton Cornak <79978722+anton-cornak@users.noreply.github.com>
2022-07-25 17:59:02 +0200

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/welcome" element={<Home />} />
 				</Routes>
+				zidan
 				<FormattedMessage id="greetings" />
 			</Router>
 		</IntlProvider>
